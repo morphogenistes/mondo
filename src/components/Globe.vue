1996711662c331ee51ae972aef5ceec95972de56
Joseph Larralde <joseph.larralde@gmail.com>
2023-10-15 23:28:52 +0200

    just a starter project made with create-vue

---
+++ src/components/Globe.vue
@@ -0,0 +1,20 @@
+<script setup>
+import ThreeGlobe from 'three-globe';
+</script>
+
+<template>
+  Gimme the globe !
+</template>
+
+<script>
+const myGlobe = new ThreeGlobe()
+  .globeImageUrl('//unpkg.com/three-globe/example/img/earth-night.jpg');
+const myScene = new THREE.Scene();
+myScene.add(myGlobe);
+export default {
+
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/GlobeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Globe from '../components/Globe.vue'
+</script>
+
+<template>
+  <div class="globe">
+    <Globe />
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
